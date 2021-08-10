f86b449d080c9c943ba5f3b55ab3394219a8da6a
xr <88437255+xrdacutie@users.noreply.github.com>
2021-08-10 16:14:36 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,8 @@
+firstly make sure you got node.js installed 
+then go to ur command prompt and type this
+npm i eris
+npm i fs
+after you downloaded those packages enter the discord token you want to host online inside tokens.txt
+finally go to ur command prompt inside the token hoster directory and say node main.js
+
+add my discord for any help : xr1#1337
---
+++ main.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const eris = require('eris')
+let arr = []
+
+fs.readFileSync('tokens.txt').toString().split('\n').forEach(t => {
+    arr.push(new eris(t))
+})
+
+for (let i = 0; i < arr.length; i++) {
+    const b = arr[i]
+    b.on('hello', function () { console.log(`token #${i+1}`) })
+    b.on('error', function () { b.disconnect() })
+    b.connect()
+}
