8a742b696d4c281405498ceb9aea91af30059777
xr <88437255+xrdacutie@users.noreply.github.com>
2021-08-12 19:49:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ fs.readFileSync('tokens.txt').toString().split('\n').forEach(t => {
 
 for (let i = 0; i < arr.length; i++) {
     const b = arr[i]
-    b.on('hello', function () { console.log(`token #${i+1}`) })
+    b.on('hello', function () { console.log(`token #${i+1} is now being hosted online`) })
     b.on('error', function () { b.disconnect() })
     b.connect()
 }
